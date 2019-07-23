2e08829c98c603f11da272a5f47b182c9eb1e386
gk0810 <53208199+gk0810@users.noreply.github.com>
2019-07-23 11:30:52 +0530

    Create guru.c

---
+++ guru.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+int guru;
+scanf("%d",&guru);
+if(guru>0)
+{
+printf("Positive");
+}
+else if(guru<0)
+{
+printf("Negative");
+}
+else
+{
+printf("zero");
+}
+return 0;
+}
